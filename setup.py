7046ec0f0fbdff88faaae2d9c3d56448a411012b
Anton Zelenin <ntnzelenin@gmail.com>
2020-12-17 17:08:53 +0200

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    version='0.1.0'
+    version='0.1.0',
+    install_requires=[
+      'requests==2.25.0',
+      'inject==4.3.1',
+    ],
 )
