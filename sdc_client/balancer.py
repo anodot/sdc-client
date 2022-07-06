91fabc14aa2cd71232e0d4d6be12179b53b7506e
Vladyslav Poliukh <vpoliukh@anodot.com>
2022-07-06 19:46:19 +0300

    [AL-10018] Add async rebalance

--- sdc_client/balancer.py
+++ sdc_client/balancer.py
@@ -12,12 +12,37 @@ class StreamsetsBalancer:
     def __init__(self):
         self.streamsets_pipelines: Dict[IStreamSets, List[IPipeline]] = get_streamsets_pipelines()
 
-    def balance(self):
-        while not self.is_balanced():
+    def balance(self, asynchronous: bool = False):
+        if not asynchronous:
+            self._perform_rebalance()
+        else:
+            self._perform_rebalance_async()
+
+    def _perform_rebalance(self):
+        while not self.is_balanced(self.streamsets_pipelines):
             pipeline = self.streamsets_pipelines[most_loaded_streamsets(self.streamsets_pipelines)].pop()
             to_streamsets = least_loaded_streamsets(self.streamsets_pipelines)
             self._move(pipeline, to_streamsets)
             self.logger.info(f'Moved `{pipeline.get_id()}` to `{pipeline.get_streamsets().get_url()}`')
+
+    def _perform_rebalance_async(self):
+        # prepare rebalance_map
+        streamsets_pipelines = self.streamsets_pipelines.copy()
+        rebalance_map = {}
+        while not self.is_balanced(streamsets_pipelines):
+            pipeline = streamsets_pipelines[most_loaded_streamsets(streamsets_pipelines)].pop()
+            to_streamsets = least_loaded_streamsets(streamsets_pipelines)
+            streamsets_pipelines[to_streamsets].append(pipeline)
+            rebalance_map[pipeline] = to_streamsets
+
+        # actual move
+        client.move_to_streamsets_async(rebalance_map)
+
+        # save pipeline with new streamsetes
+        for pipeline in rebalance_map:
+            pipeline.set_streamsets(rebalance_map[pipeline])
+            self.pipeline_provider.save(pipeline)
+        self.streamsets_pipelines = streamsets_pipelines.copy()
 
     def unload_streamsets(self, streamsets: IStreamSets):
         for pipeline in self.streamsets_pipelines.pop(streamsets):
@@ -37,11 +62,12 @@ class StreamsetsBalancer:
             client.start(pipeline)
         self.streamsets_pipelines[to_streamsets].append(pipeline)
 
-    def is_balanced(self) -> bool:
-        if len(self.streamsets_pipelines.keys()) < 2:
+    @staticmethod
+    def is_balanced(streamsets_pipelines: dict) -> bool:
+        if len(streamsets_pipelines) < 2:
             return True
         # streamsets are balanced if the difference in num of their pipelines is 0 or 1
-        lengths = [len(pipelines) for pipelines in self.streamsets_pipelines.values()]
+        lengths = [len(pipelines) for pipelines in streamsets_pipelines.values()]
         return max(lengths) - min(lengths) < 2
 
 
